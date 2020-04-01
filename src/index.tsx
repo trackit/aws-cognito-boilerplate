e94d2c7c4dee03d13d11e780b3da274fd6659353
Nathan Agez <nathan.agez@gmail.com>
2020-04-01 13:07:53 +0200

    refactor(core): Fix .gitignore and document example.env
    Cypress videos and screenshots folders were not part of .gitignore, they have been added.
    Documented routes.config.ts and example.env files.

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { routes } from "shared/routes.config";
 
 ReactDOM.render(
--- src/shared/routes.config.ts
+++ src/shared/routes.config.ts
@@ -1,6 +1,7 @@
 import { Login, Register } from "pages";
 import { RouteProps } from "react-router-dom";
 
+// Theses routes are used inside index.tsx by react-router-dom
 export const routes: RouteProps[] = [
   { path: "/", component: Login, exact: true },
   { path: "/register", component: Register }
