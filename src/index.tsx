eb228e353c1d3113ad58454421ea77a325fb673a
Nathan Agez <nathan.agez@gmail.com>
2020-04-10 15:54:42 +0200

    refactor(amplify): Remove unused expressions

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import * as serviceWorker from "./serviceWorker";
-import React, { Props } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import App from "App";
 import { GlobalStyle } from "shared/styles";
