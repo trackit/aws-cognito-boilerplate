3fad60e6a5478463fbe64d5de0562dbf907e691f
Nathan Agez <nathan.agez@gmail.com>
2020-04-14 10:17:22 +0200

    refactor(Amplify):
    - Remove color palettes
    - Remove react-router-dom from dependencies
    - Refactor interface names

--- src/shared/interfaces/styles.interface.ts
+++ src/shared/interfaces/styles.interface.ts
@@ -1,45 +1,6 @@
 /**
- * Used by Colors
+ * Used for theming
  */
-interface Elevation {
-  "50": string;
-  "100": string;
-  "200": string;
-  "300": string;
-  "400": string;
-  "500": string;
-  "600": string;
-  "700": string;
-  "800": string;
-  "900": string;
-  a100?: string;
-  a200?: string;
-  a400?: string;
-  a700?: string;
-}
-
-export interface PlatteColors {
-  red: Elevation;
-  pink: Elevation;
-  purple: Elevation;
-  deeppurple: Elevation;
-  indigo: Elevation;
-  blue: Elevation;
-  lightblue: Elevation;
-  cyan: Elevation;
-  teal: Elevation;
-  green: Elevation;
-  lightgreen: Elevation;
-  lime: Elevation;
-  yellow: Elevation;
-  amber: Elevation;
-  orange: Elevation;
-  deeporange: Elevation;
-  brown: Elevation;
-  grey: Elevation;
-  bluegrey: Elevation;
-}
-
 export interface Colors {
   primary: string;
   secondary: string;
@@ -51,23 +12,3 @@ export interface Colors {
   warning: string;
   danger: string;
 }
-
-/* export interface AppColors {
-  [ColorName: string]: {
-    "50": string;
-    "100": string;
-    "200": string;
-    "300": string;
-    "400": string;
-    "500": string;
-    "600": string;
-    "700": string;
-    "800": string;
-    "900": string;
-    a100?: string;
-    a200?: string;
-    a400?: string;
-    a700?: string;
-  };
-}
- */
