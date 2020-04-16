d93a7dd75fa39ed297de9f6616c902d67d400990
Nathan Agez <nathan.agez@gmail.com>
2020-04-16 17:36:30 +0200

    feat(reset-password): Add forget password component
    - Add capitalize first letter util

--- src/shared/components/Auth/index.ts
+++ src/shared/components/Auth/index.ts
@@ -1,5 +1,6 @@
 import CustomConfirmSignUp from "./ConfirmSignUp";
 import CustomSignIn from "./SignIn";
 import CustomSignUp from "./SignUp";
+import CustomForgotPassword from "./ForgotPassword";
 
-export { CustomConfirmSignUp, CustomSignIn, CustomSignUp };
+export { CustomConfirmSignUp, CustomSignIn, CustomSignUp, CustomForgotPassword };
--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -2,6 +2,14 @@ import Amplify from "aws-amplify";
 import { AuthState, AmplifyConfig } from "shared/interfaces/amplify.interface";
 
 /**
+ * This function is used to capiatalize first letter of a string
+ * @param {string} str string to capitalize
+ */
+export const capitalizeFirstLetter = (str: string) => {
+  return str[0].toUpperCase() + str.slice(1);
+};
+
+/**
  * This function is used to control if the received
  * state is equal to the signedIn value.
  *
@@ -45,11 +53,11 @@ export class Config extends Amplify {
   public async init(amplifyConfig: AmplifyConfig) {
     try {
       /**
-       * We unfornately have to apply this workaround because Webpack performs 
+       * We unfornately have to apply this workaround because Webpack performs
        * a static analyse at build time. It doesn't try to infer variables.
        * For more information check this issue: https://github.com/webpack/webpack/issues/6680#issuecomment-370800037
        */
-      const name = 'aws-exports'
+      const name = "aws-exports";
       const module = await import(`../${name}`);
       Config.configure(module.default);
     } catch (error) {
