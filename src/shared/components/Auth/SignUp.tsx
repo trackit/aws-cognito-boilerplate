273ae9ce4f287962bbfa0a161c12f41e18829c35
Nathan Agez <nathan.agez@gmail.com>
2020-04-16 11:21:48 +0200

    fix(imports): Fix unused imports

--- src/shared/components/Auth/SignUp.tsx
+++ src/shared/components/Auth/SignUp.tsx
@@ -7,9 +7,8 @@ import {
   Form,
   Button,
   FormField,
-  Logo,
   CustomLink,
-  Container
+  Container,
 } from "shared/components/Auth/common";
 import styled from "styled-components";
 
@@ -28,52 +27,54 @@ class CustomSignUp extends SignUp {
   showComponent(theme: AmplifyTheme) {
     this.sortFields();
     return (
-      <Form>
-        {/**
-         * As a workaround we use a children because we can't add
-         * more props from the time that SignUp class is not a generic class.
-         */}
-        {this.props.children}
-        {this.signUpFields.map((field) => {
-          return (
-            field.key !== "phone_number" && (
-              <FormField key={field.key}>
-                <label htmlFor={field.key}>
-                  {field.required ? `${field.label} *` : field.label}
-                </label>
-                <Input
-                  id={field.key}
-                  autoFocus={
-                    this.signUpFields.findIndex((f) => {
-                      return f.key === field.key;
-                    }) === 0
-                      ? true
-                      : false
-                  }
-                  placeholder={field.placeholder}
-                  theme={theme}
-                  type={field.type}
-                  name={field.key}
-                  key={field.key}
-                  onChange={this.handleInputChange}
-                />
-              </FormField>
-            )
-          );
-        })}
-        <Button
-          disabled={this.state.requestPending}
-          onClick={(ev) => {
-            ev.preventDefault();
-            this.signUp();
-          }}
-        >
-          Sign Up
-        </Button>
-        <CustomLink onClick={() => this.changeState("signIn")}>
-          Have an account? Sign In
-        </CustomLink>
-      </Form>
+      <SingUpContainer>
+        <Form>
+          {/**
+           * As a workaround we use a children because we can't add
+           * more props from the time that SignUp class is not a generic class.
+           */}
+          {this.props.children}
+          {this.signUpFields.map((field) => {
+            return (
+              field.key !== "phone_number" && (
+                <FormField key={field.key}>
+                  <label htmlFor={field.key}>
+                    {field.required ? `${field.label} *` : field.label}
+                  </label>
+                  <Input
+                    id={field.key}
+                    autoFocus={
+                      this.signUpFields.findIndex((f) => {
+                        return f.key === field.key;
+                      }) === 0
+                        ? true
+                        : false
+                    }
+                    placeholder={field.placeholder}
+                    theme={theme}
+                    type={field.type}
+                    name={field.key}
+                    key={field.key}
+                    onChange={this.handleInputChange}
+                  />
+                </FormField>
+              )
+            );
+          })}
+          <Button
+            disabled={this.state.requestPending}
+            onClick={(ev) => {
+              ev.preventDefault();
+              this.signUp();
+            }}
+          >
+            Sign Up
+          </Button>
+          <CustomLink onClick={() => this.changeState("signIn")}>
+            Have an account? Sign In
+          </CustomLink>
+        </Form>
+      </SingUpContainer>
     );
   }
 }
